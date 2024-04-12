01627f4d389ddf18d66d0eba8693ba71f9633ae2
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-04-12 21:25:18 +0530

    Create pages and add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,42 @@
-const App = () => {
-    return <h1 className="">app</h1>;
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ReactElement } from "react";
 
-export default App;
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+export default function App(): ReactElement {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/about"
+                    element={<About />}
+                />
+                <Route
+                    path="/projects"
+                    element={<Projects />}
+                />
+                <Route
+                    path="/sign-in"
+                    element={<SignIn />}
+                />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                />
+                <Route
+                    path="/dashboard"
+                    element={<Dashboard />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
